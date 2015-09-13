7ac6985cdc8bac711c42599e86c5c9cc46e8ab4e
denglin93 <284309165@qq.com>
2015-09-13 14:39:42 +0800

    xmlDOm解析步骤

--- xmlDemo/src/com/oracle/wdp/xml/dom/DOMTest.java
+++ xmlDemo/src/com/oracle/wdp/xml/dom/DOMTest.java
@@ -13,23 +13,23 @@ public class DOMTest {
 	public static void main(String[] args) {
 		// 获取DocumentBuilderFacotry 对象
 		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-		// 创建DocumentBuilder对象
 		try {
-			// 加载xml文档
+			// 创建DocumentBuilder对象
 			DocumentBuilder db = dbf.newDocumentBuilder();
+			//通过DocumentBuilder加载xml文档得到document对象
 			Document document = db.parse("book.xml");
-			// 获取节点
+			// 通过document.getElementsByTagName()来获取多个节点NodeList bookList
 			NodeList bookList = document.getElementsByTagName("book");
 			System.out.println("一共有" + bookList.getLength() + "本书");
 			for (int i = 0; i < bookList.getLength(); i++) {
-				// 获取一个book节点
+				// 通过bookList.item(i)遍历获取每一个book节点,得到Node类型的book
 				Node book = bookList.item(i);
 				System.out.println("==============下面开始遍历第" + (i + 1)
 						+ "本书，书的内容是:==============");
-				// 获取book一个节点的所有属性集合
+				// 通过book.getAttributes()来获取一个节点的所有属性集合得到NamedNodeMap类型attrs
 				NamedNodeMap attrs = book.getAttributes();
 				for (int j = 0; j < attrs.getLength(); j++) {
-					// 通过item方法获取某一个属性
+					// 通过遍历attrs.item()方法获取某一个属性Node类型的node ，从而根据node.getNodeName()和node.getNodeValue()来获取节点名称和值
 					Node node = attrs.item(j);
 			
 					System.out.println("属性名：" + node.getNodeName());
@@ -38,6 +38,7 @@ public class DOMTest {
 				}
 				System.out.println("==============下面结束遍历第" + (i + 1)
 						+ "本书:==============");
+				//通过book.getChildNodes()来获取所有的子节点返回NodeList类型的childNodes
 				NodeList childNodes = book.getChildNodes();
 				for (int j = 0; j < childNodes.getLength(); j++) {
 					if (childNodes.item(j).getNodeType() == Node.ELEMENT_NODE) {
