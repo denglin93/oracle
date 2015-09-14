50431c6c7f0c8da441be2c38aa8b20e9e1cb093a
denglin93 <284309165@qq.com>
2015-09-14 23:05:28 +0800

    完整版的xml读取与写入

--- xmlDemoWrite/src/com/oracle/wdp/xml/dom4j/DOM4JTest.java
+++ xmlDemoWrite/src/com/oracle/wdp/xml/dom4j/DOM4JTest.java
@@ -1,25 +1,30 @@
 package com.oracle.wdp.xml.dom4j;
 
 import java.io.File;
-
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 import org.dom4j.Attribute;
 import org.dom4j.Document;
+import org.dom4j.DocumentHelper;
 import org.dom4j.Element;
+import org.dom4j.io.OutputFormat;
 import org.dom4j.io.SAXReader;
+import org.dom4j.io.XMLWriter;
 
 import com.oracle.wdp.xml.dom4j.entity.copy.Book;
 
 public class DOM4JTest {
 	private static List<Book> list = new ArrayList<Book>();
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//得到SAXReader reader 对象
+
+	public void parseXML() {
+		// 得到SAXReader reader 对象
 		SAXReader reader = new SAXReader();
-		try {// 通过reader对象的reader方法加载book.xml文件，获取document对象reader.read(new File("book.xml"))
+		try {// 通过reader对象的reader方法加载book.xml文件，获取document对象reader.read(new
+				// File("book.xml"))
 			Document document = reader.read(new File("book.xml"));
 			// 通过document对象获取跟节点document.getRootElement()来获取Element bookStore
 			Element bookStore = document.getRootElement();
@@ -29,7 +34,7 @@ public class DOM4JTest {
 			while (it.hasNext()) {
 				System.out.println("===============开始遍历某一本书================");
 				Book bookEntity = new Book();
-				//通过迭代来获取Element book
+				// 通过迭代来获取Element book
 				Element book = (Element) it.next();
 				List<Attribute> bookAttrs = book.attributes();
 				for (Attribute attr : bookAttrs) {
@@ -39,7 +44,7 @@ public class DOM4JTest {
 						bookEntity.setId(attr.getValue());
 					}
 				}
-				//通过book.elementIterator()来得到Iterator itt
+				// 通过book.elementIterator()来得到Iterator itt
 				Iterator itt = book.elementIterator();
 
 				while (itt.hasNext()) {
@@ -68,6 +73,40 @@ public class DOM4JTest {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+
+	public void createXML() {
+		// 1.创建document对象，代表整个xml文档
+		Document document = DocumentHelper.createDocument();
+		// 2.创建根节点rss
+		Element rss = document.addElement("rss");
+		// 3.向rss中添加version属性
+		rss.addAttribute("version", "2.0");
+		// 4.生成子节点以及内容
+		Element channel = rss.addElement("channel");
+		Element title = channel.addElement("title");
+		title.setText("国内最新新闻");
+		//5.设置生车xml的格式
+		OutputFormat format = OutputFormat.createPrettyPrint();
+		format.setEncoding("utf-8");
+		//6.生成xml文件
+		File file = new File("rssnews.xml");
+		try {
+			XMLWriter writer = new XMLWriter(new FileOutputStream(file),format);
+			//设置是否转义
+			writer.setEscapeText(false);
+			writer.write(document);
+			writer.close();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DOM4JTest d = new DOM4JTest();
+		d.createXML();
 	}
 
 }
--- xmlDemoWrite/src/com/oracle/wdp/xml/jdom/JDOMTest.java
+++ xmlDemoWrite/src/com/oracle/wdp/xml/jdom/JDOMTest.java
@@ -1,6 +1,8 @@
 package com.oracle.wdp.xml.jdom;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
@@ -9,12 +11,15 @@ import org.jdom2.Attribute;
 import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.input.SAXBuilder;
+import org.jdom2.output.Format;
+import org.jdom2.output.XMLOutputter;
 
 import com.oracle.wdp.xml.jdom.entity.Book;
 
 public class JDOMTest {
 	private static List<Book> booksList = new ArrayList<Book>();
-	public static void main(String[] args) {
+
+	public void parseXML() {
 		// 对books.xml进行JDOM解析
 		// 创建一个SAXBuilder对象new SAXBuilder()；
 		SAXBuilder saxBuilder = new SAXBuilder();
@@ -30,51 +35,87 @@ public class JDOMTest {
 
 			for (Element book : bookList) {
 				Book bookEntity = new Book();
-				System.out.println("===========开始解析第" + (bookList.indexOf(book)+1)
-						+ "书=========");
-				
-				//解析book的属性集合book.getAttributes()来得到List<Attribute>类型attrList
+				System.out.println("===========开始解析第"
+						+ (bookList.indexOf(book) + 1) + "书=========");
+
+				// 解析book的属性集合book.getAttributes()来得到List<Attribute>类型attrList
 				List<Attribute> attrList = book.getAttributes();
-				
-				//知道属性名获取值
+
+				// 知道属性名获取值
 				String idValue = book.getAttributeValue("id");
-	
-				//遍历for (Attribute attr : attrList)
+
+				// 遍历for (Attribute attr : attrList)
 				for (Attribute attr : attrList) {
-				String attrName=attr.getName();
-				String attrValue = attr.getValue();
-				System.out.println("属性名："+attrName+"----属性值:"+attrValue);
-					if("id".equals(attrName)){
+					String attrName = attr.getName();
+					String attrValue = attr.getValue();
+					System.out.println("属性名：" + attrName + "----属性值:"
+							+ attrValue);
+					if ("id".equals(attrName)) {
 						bookEntity.setId(attrValue);
-					}	
+					}
 				}
-				//通过book.getChildren()来获取所有的节点集合List<Element> bookChilds
-				List<Element> bookChilds =book.getChildren();
+				// 通过book.getChildren()来获取所有的节点集合List<Element> bookChilds
+				List<Element> bookChilds = book.getChildren();
 				for (Element child : bookChilds) {
-					System.out.println("节点名："+child.getName()+"---节点值:"+child.getValue());
-				if("name".equals(child.getName())){
+					System.out.println("节点名：" + child.getName() + "---节点值:"
+							+ child.getValue());
+					if ("name".equals(child.getName())) {
 						bookEntity.setName(child.getValue());
-				}else if("author".equals(child.getName())){
-					bookEntity.setAuthor(child.getValue());
-				}else if("year".equals(child.getName())){
-					bookEntity.setYear(child.getValue());
-				}else if("price".equals(child.getName())){
-					bookEntity.setPrice(child.getValue());
-				}else if("Language".equals(child.getName())){
-					bookEntity.setLanguage(child.getValue());
+					} else if ("author".equals(child.getName())) {
+						bookEntity.setAuthor(child.getValue());
+					} else if ("year".equals(child.getName())) {
+						bookEntity.setYear(child.getValue());
+					} else if ("price".equals(child.getName())) {
+						bookEntity.setPrice(child.getValue());
+					} else if ("Language".equals(child.getName())) {
+						bookEntity.setLanguage(child.getValue());
+					}
+
+					System.out.println("===========结束解析第"
+							+ (bookList.indexOf(book) + 1) + "书=========");
+					booksList.add(bookEntity);
+					bookEntity = null;
 				}
-					
-				System.out.println("===========结束解析第" + (bookList.indexOf(book)+1)
-						+ "书=========");
-				booksList.add(bookEntity);
-				bookEntity = null;
 			}
-				}
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
+	}
+	public void createXML(){
+		//1.生成一个根节点
+		Element rss= new Element("rss");
+		//2.添加属性
+		rss.setAttribute("version", "2.0");
+		//3.生成一个document对象
+		Document document = new Document(rss);
+		
+		Element channel = new Element("channle");
+		rss.addContent(channel);
+		Element title = new Element("title");
+		title.setText("<>国际新闻");
+		channel.addContent(title);
+		
+		Format format = Format.getPrettyFormat();
+		format.setIndent("");
+		format.setEncoding("utf-8");
+		
+		//4.创建XMLOutputter 对象
+		XMLOutputter outputer = new XMLOutputter(format);
+		try {
+			outputer.output(document, new FileOutputStream(new File("rssnews1.xml")));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		//
+		
+	}
+	public static void main(String[] args) {
+JDOMTest j = new JDOMTest();
+j.createXML();
 	}
 
 }
